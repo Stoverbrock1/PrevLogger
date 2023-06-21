8f056609848894f2b8b8f2395896da1f74313b8f
Stoverbrock1 <ztgstover@berkeley.edu>
2023-06-21 12:32:44 -0600

    debugging even more

--- Archive_watcher.py
+++ Archive_watcher.py
@@ -180,7 +180,7 @@ def update_archive(file_list):
                 'kurtosis': row["kurtosis"]
             })
             data_list.update({index : metadata})
-    #print(data_list)
+    print(data_list)
     return data_list
 
 if __name__ == '__main__':
