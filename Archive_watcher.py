aba97e2e444966cdfa458a8599360e31d91bcba4
Stoverbrock1 <ztgstover@berkeley.edu>
2023-06-22 13:41:26 -0600

    print datalist

--- Archive_watcher.py
+++ Archive_watcher.py
@@ -186,7 +186,7 @@ def update_archive(file_list):
             #print(metadata)
             data_list.update({index : dict(metadata)})
             #data_list[index] = metadata
-    #print(data_list)
+    print(data_list)
     return data_list
 
 if __name__ == '__main__':
